e6d278d8040c38f90e236376ab1ede6f264d9945
Haiou1220 <38627132+Haiou1220@users.noreply.github.com>
2018-10-26 20:58:46 +0800

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,77 +1,90 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
-// 写一个判断的括号是否匹配的函数
-int MatchCheck(char a[],int len){
-    int flag = 0;
-    char s[10000];
-    int top,i;
-    char temp;
-    // 初始化一个栈
-    top = 0;
-    for(i=0;i<len;i++){
-        if(a[i]=='['){ // 如果是左括号直接入栈
-            s[++top]=a[i];
-            continue;
-        }
-        if(a[i]==']'){ // 如果是右括号，则尝试匹配
-            temp = s[top];
-            if(temp=='['){
-                flag = 1;
-                top--;
-                continue;
-            }else{
-                flag = 0;
-                break;
-            }
-        }
+#include<string.h>
 
-        if(a[i]=='('){ // 如果是左括号直接入栈
-            s[++top]=a[i];
-            continue;
-        }
-        if(a[i]==')'){ // 如果是右括号，则尝试匹配
-            temp = s[top];
-            if(temp=='('){
-                flag = 1;
-                top--;
-                continue;
-            }else{
-                flag = 0;
-                break;
-            }
-        }
-    }
-    if(flag&&(top==0)){
-        return 1;
-    }else{
-        return 0;
-    }
+#define  n_group_max 10000
+#define  n_group_elem_max 1000 
+#define  n_elem_rect_max  10000
 
+#define  Error_Print( ) ;
+
+typedef struct str_rect{
+	int l;
+	int w;
+}*p_str_rect_type,str_rect_type;
+
+int input_n_group(unsigned int *p_n_group);
+int input_n_group_elem_num( int n_group_site, int *p_elem_num);
+
+ input_elem_rectangle(p_str_rect_type  rect);
+
+int main(void)
+{
+	unsigned int n_group;/*多少组测试数据*/
+	int* p_elem_num;/*每一组有多少个长方形*/
+	
+    p_elem_num =  input_n_group(&n_group);
+    input_n_group_elem_num( n_group , &p_elem_num);
+     input_elem_rectangle(p_str_rect_type  rect)
+    
+	//scanf("%d",n_group_elem);
+//	scanf("%d",elem_rect);
+ //	printf("ok");
+	return 0;
 }
-int main(){
-    // 提示输入一个数
-    int N,i;
-    scanf("%d",&N);
-    int len;
-     // 对N组数据循环判断
-     int count = 1;
-     int result[5];
-     while(count<=N){
-         char a[10000]={'\0'};
-        // 读入一行字符串
-        scanf("%s",&a);
-         // 求字符串的长度
-        len = strlen(a);
-        result[count]=MatchCheck(a,len);
-        count++;
-     }
-     for(i=1;i<count;i++){
-         if(result[i]){
-             printf("Yes\n");
-         }else{
-             printf("No\n");
-         }
-     }
-     return 0;
-}//{[({()()()({}{}{})})]}
+
+int* input_n_group(unsigned int *p_n_group)
+{
+	int n_size;
+	int *p;
+	  
+	scanf("%d",p_n_group);
+	if(*p_n_group>=n_group_max)
+	{	
+		exit(-1) ;
+	}else
+	{	
+		n_size = sizeof(int)*(*p_n_group); /*每一组的长方形个数不超过10 000*/
+		p = malloc( n_size);
+		memset(p,0,n_size);
+		return p;
+	}	
+	
+ } 
+ 
+ int input_n_group_elem_num( int n_group_site, int *p_elem_num)
+{
+	int test_elem_num;
+	for(i=0;i<n_group_site;i++) 
+	{	
+	 do
+	 {
+	 scanf("%d",&test_elem_num);
+	 } while(test_elem_num>n_group_elem_max);
+	 
+	 p_elem_num[i] = test_elem_num;
+		
+	}
+	return 0;
+ }
+ 
+ input_elem_rectangle(p_str_rect_type  rect)
+ {
+ 	int test_rectangle_val;
+ 	
+ 	
+ 	for(i=0;i<2;i++) /*一个长方形元素输入wide与high*/
+	{
+		do
+		 {
+		 	scanf("%d",&test_rectangle);
+		 	if(test_rectangle_val>n_elem_rect_max)  Error_Print(rectangle value to longger);
+		 } while(test_rectangle_val>n_elem_rect_max);
+		 
+		 if(0==i) rect->l = test_rectangle;
+		 else rect->w = test_rectangle;
+		
+	}
+ 	
+ 	
+ }//////
